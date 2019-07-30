aae525332321b5339f69f6a0f216b6adb280884a
JVFayad <43392012+JVFayad@users.noreply.github.com>
2019-07-29 22:39:04 -0300

    alguns conceitos nomeados e exemplificados

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
 // elemento
-const elemento = <h2>Este é um elemento!</h2>;
+const elemento = <h2>Este é um elemento JSX!</h2>; // JSX
+const elemento2 = React.createElement(
+  "div",
+  null,
+  React.createElement("h2", null, "Este é um elemento!")
+);
 
-// componente
-function App() {
+// arrow function
+const Foo = props => {
+  return <p>Outro desse {props.number}</p>;
+};
+
+// componente: functional component
+function App(props) {
+  const [i, setI] = useState(2);
+
+  const increment = () => {
+    setI(i + 2);
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello {props.name}</h1>
+      <h1>Números pares {i}</h1>
+      <button onClick={increment}>Clique aqui</button>
+      <Foo number={i} />
       <h2>Start editing to see some magic happen!</h2>
       {elemento}
+      {elemento2}
     </div>
   );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App name="João" />, rootElement);
